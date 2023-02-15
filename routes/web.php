53b2d382aa71c7a8491d4959f7cfa7bfb20d71f4
Zayar-Shane <zeyarhtet.mgk@gmail.com>
2023-02-15 21:27:45 +0630

    db realation"

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,3 +29,19 @@ Route::get('post/updatePage/{id}', [PostController::class, 'updatePage'])->name(
 Route::get('post/editPage/{id}', [PostController::class, 'editPage'])->name('post#editPage');
 // Route::post('post/update/{id}', [PostController::class, 'update'])->name('post#update');
 Route::post('post/update', [PostController::class, 'update'])->name('post#update');
+
+// Database Relation test
+Route::get('product/list', function () {
+    $data = Product::select('products.*', 'categories.name as category_name', 'categories.description')
+        ->rightJoin('categories', 'products.category_id', 'categories.id')
+        ->get();
+    dd($data->toArray());
+});
+
+Route::get('order/list', function () {
+    $data = Order::select('customers.name as customer_name', 'products.name as product_name')
+        ->join('customers', 'orders.customer_id', 'customers.id')
+        ->join('products', 'orders.product_id', 'products.id')
+        ->get();
+    dd($data->toArray());
+});
